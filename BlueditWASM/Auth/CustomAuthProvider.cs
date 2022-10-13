383c95ecd88a06dcfd609da0669513b4fe358c33
Mads Hylleberg Nielsen <madshyllebergn@gmail.com>
2022-10-13 18:13:37 +0200

    / ¤ Version 0.3 - Now with SIGNUP function! + page / # Completed the setup for a Signup page.
    / ! TO BE ADDED
    / * Create post
    / * View posts
    / * View my posts
    / * Setup for Admin/User roles.
    / * Total access to all the different API methods in Blazor
    / * A good looking UI!

---
+++ BlueditWASM/Auth/CustomAuthProvider.cs
@@ -0,0 +1,36 @@
+﻿using System.Security.Claims;
+using BlueditWASM.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlueditWASM.Auth;
+
+public class CustomAuthProvider: AuthenticationStateProvider
+{
+    // # Fields
+    private readonly IAuthService authService;
+
+    // ¤ Constructor
+    public CustomAuthProvider(IAuthService authService)
+    {
+        this.authService = authService;
+        authService.OnAuthStateChanged += AuthStateChanged;
+    }
+    
+    // ¤ Get the current Auth state.
+    public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        ClaimsPrincipal principal = await authService.GetAuthAsync();
+        
+        return new AuthenticationState(principal);
+    }
+    
+    // ¤ Auth State changed (Notify when state changed!)
+    private void AuthStateChanged(ClaimsPrincipal principal)
+    {
+        NotifyAuthenticationStateChanged(
+            Task.FromResult(
+                new AuthenticationState(principal)
+            )
+        );
+    }
+}
--- BlueditWASM/Program.cs
+++ BlueditWASM/Program.cs
@@ -1,6 +1,10 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlueditWASM;
+using BlueditWASM.Auth;
+using BlueditWASM.Services.Http;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,8 +12,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // ¤ Added by ME
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7157") });
-
-
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+AuthorizationPolicies.AddPolicies(builder.Services);
 
 
 await builder.Build().RunAsync();
