568200ba4a7c073af7e1c660c8db061b3c80f7d3
Mads Hylleberg Nielsen <madshyllebergn@gmail.com>
2022-10-26 18:10:17 +0200

    / ¤ Version 0.7 - Fixed Blazor / * Reworked the ENTIRE Blazor....
    / < TO BE ADDED
    / * Setup for Admin/User roles...
    / * Users being able to delete/edit their own posts.
    / * Admin menu for deleting posts from other users.
    / * A good looking UI!

---
+++ BlueditBlazor/Program.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlueditBlazor;
+using BlueditBlazor.Auth;
+using Http;
+using HttpClients.Interfaces;
+using HttpClients.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using Shared.Auth;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+
+
+
+// ¤ Added by ME
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7157") });
+builder.Services.AddScoped<IUserService, UserHttpClient>();
+builder.Services.AddScoped<IPostService, PostHttpClient>();
+
+// ! Authentication
+builder.Services.AddScoped<IAuthService, JwtAuthService>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthProvider>();
+AuthorizationPolicies.AddPolicies(builder.Services);
+
+await builder.Build().RunAsync();
